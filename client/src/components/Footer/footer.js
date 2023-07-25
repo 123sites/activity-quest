9fdd493d483c78c3c9a0367582a3d9cf09d11a66
CF <cfreitas12345@gmail.com>
2023-07-24 19:16:57 -0700

    Added component folders & files.  Added to the footer.

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "../../assets/style.css";
+
+const Footer = () => {
+  return (
+    <footer className="text-center mt-3">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/chelwebdeveloper"
+      >
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/123sites/bark-buddy"
+      >
+        <i className="fab fa-github"></i>
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="mailto:cfreitas12345@gmail.com"
+      >
+        <i className="fas fa-envelope"></i>
+      </a>
+      <h4 className="made my-3 fw-bolder fs-7">
+        🌟 Activity Quest{" "}
+        <span
+          className="emoji"
+          role="img"
+          aria-label="Stars"
+          aria-hidden="false"
+        >
+          🌟
+        </span>
+        {""}
+      </h4>
+      <p>
+        <span className="copyright">
+          Copyright © Chel Freitas and Gayle Hoefer | C & G, Inc.
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Theme/themeContext.js
@@ -0,0 +1,22 @@
+// ThemeContext.js
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    document.body.setAttribute(
+      "data-theme",
+      theme === "light" ? "dark" : "light"
+    );
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
