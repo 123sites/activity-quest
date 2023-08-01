1b09812e6cd9c50f7156be84b8bb4cc4826ea9fd
CF <cfreitas12345@gmail.com>
2023-07-31 20:06:53 -0700

    Set-up the Route path for Home & installed Router-DOM.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Calender from "./components/Calender/calender";
+import Home from "./pages/Home.js";
 
-export default function App() {}
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/calender" element={<Calender />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
