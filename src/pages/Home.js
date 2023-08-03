058f01b9b41d52a8bcd2b57783a67a78fd47f38e
CF <cfreitas12345@gmail.com>
2023-08-02 17:04:01 -0700

    Set-up the css default, created home.css, template for home & navbar.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useContext } from "react";
 import { ThemeContext } from "../components/Theme/themeContext";
 import "../App.css";
+import "../index.css";
 
 const Home = ({ handlePageChange }) => {
-  const { toggleTheme, theme } = useContext(ThemeContext);
   const [currentPath, setCurrentPath] = useState("");
+  console.log(currentPath);
 
   useEffect(() => {
     console.log(currentPath);
@@ -14,9 +15,61 @@ const Home = ({ handlePageChange }) => {
 
   return (
     <main>
-      <div
-        className={`container-fluid mt-4 shadow-lg homeBox-${theme} vContainer-${theme}`}
-      ></div>
+      <div id="myCarousel" className="carousel slide" data-ride="carousel">
+        {/* <!-- Indicators --> */}
+        <ol className="carousel-indicators">
+          <li
+            data-target="#myCarousel"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+        </ol>
+
+        {/* <!-- Wrapper for slides --> */}
+        <div className="carousel-inner" role="listbox">
+          <div className="item active">
+            <img src="https://placehold.it/1200x400?text=IMAGE" alt="Image" />
+            <div className="carousel-caption">
+              <h3>Text goes here.</h3>
+              <p>Paragraph.</p>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- Left and right controls --> */}
+        <a
+          className="left carousel-control"
+          href="#myCarousel"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="glyphicon glyphicon-chevron-left"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="right carousel-control"
+          href="#myCarousel"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="glyphicon glyphicon-chevron-right"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+
+      <div className="container text-center">
+        <h3>What We Do</h3>
+        <div className="row">
+          <div className="col-sm-4"></div>
+        </div>
+      </div>
     </main>
   );
 };
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useContext } from "react";
 import { ThemeContext } from "../components/Theme/themeContext";
 import "../App.css";
+import "../index.css";
 
 const Navbar = ({ handlePageChange }) => {
   const { toggleTheme, theme } = useContext(ThemeContext);
@@ -13,10 +14,47 @@ const Navbar = ({ handlePageChange }) => {
   }, []);
 
   return (
-    <nav className="flex sticky">
-      <div
-        className={`container-fluid mt-4 shadow-lg homeBox-${theme} vContainer-${theme}`}
-      ></div>
+    <nav className="navbar navbar-inverse">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            class="navbar-toggle"
+            data-toggle="collapse"
+            data-target="#myNavbar"
+          >
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">
+            Logo
+          </a>
+        </div>
+        <div className="collapse navbar-collapse" id="myNavbar">
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Account</a>
+            </li>
+            <li>
+              <a href="#">???</a>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#">
+                <span className="glyphicon glyphicon-log-in"></span> Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
