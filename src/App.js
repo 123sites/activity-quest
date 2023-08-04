39438fdb834d072f6f7e91cd73000022b01693a9
Gayle Hoefer <gaylehoefer@gmail.com>
2023-08-03 19:29:37 -0700

    added calender setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Calender from "./components/Calender/calender";
 
 export default function App() {
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/calender" element={<Calender />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
