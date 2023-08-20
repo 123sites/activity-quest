94ed16b064b1fbfa12e873a7ee3bb79a5ac98c9e
CF <cfreitas12345@gmail.com>
2023-08-20 00:23:40 -0700

    Finished the login, out, and sign-up routes.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Calender from "./components/Calender/calender";
 import Home from "./pages/Home.js";
+import Signup from "./pages/Signup.js";
+import Login from "./pages/Login.js";
 import { ThemeProvider } from "./components/Theme/themeContext";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
@@ -13,9 +15,9 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           {/* <Route path="/calender" element={<Calendar />} /> */}
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/signup" element={<Signup />} /> */}
-          {/* <Route path="/logout" element={<Login />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/logout" element={<Login />} />
           <Route path="./public/404.html">"404 Not Found!"</Route>
         </Routes>
       </ThemeProvider>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,104 +1,56 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
-import { useMutation } from "@apollo/client";
-import { ADD_USER } from "../utils/mutations";
-import Auth from "../utils/auth";
 
 const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [signedUp, setSignedUp] = useState(false);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
+  const handleSignup = () => {
+    // Simulate user registration (replace this with your actual registration logic)
+    if (password === confirmPassword) {
+      // Here, you might want to call an API to register the user
+      setSignedUp(true);
+    } else {
+      alert("Passwords do not match");
     }
   };
 
   return (
-    <main className="flex-row justify-center m-4">
-      <div className="col-10 col-lg-7">
-        <div className="card shadow-lg">
-          <h4 className="card-header shadow-lg p-3 text-center text-light login-card-header">
-            Signup
-            <i className="fa-sharp fa-solid fa-right-to-bracket ml-3 door-open"></i>
-          </h4>
-          <div className="card-body shadow-lg">
-            {data ? (
-              <p>
-                Success! You may now head{" "}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form
-                className="d-flex p-2 flex-column justify-content-center align-items-center"
-                onSubmit={handleFormSubmit}
-              >
-                <input
-                  className="form-input shadow-lg"
-                  placeholder="Your username"
-                  name="username"
-                  type="text"
-                  value={formState.name}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input shadow-lg"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input shadow-lg"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="sbtn m-3 col-2 btn-block fw-bolder fs-5"
-                  style={{ cursor: "pointer" }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger rounded text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
+    <div>
+      {signedUp ? (
+        <div>
+          <h1>Registration Successful</h1>
+          <p>Your account has been created successfully.</p>
         </div>
-      </div>
-    </main>
+      ) : (
+        <div>
+          <h1>Sign Up</h1>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <br />
+          <button onClick={handleSignup}>Sign Up</button>
+        </div>
+      )}
+    </div>
   );
 };
 
