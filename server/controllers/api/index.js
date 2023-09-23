c281e00b2431658bc4f1ebf1a934d194b073a766
CF <cfreitas12345@gmail.com>
2023-09-23 16:22:51 -0700

    Created the singleEventRoute.js.

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -3,13 +3,13 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 const accountRoutes = require("./accountRoutes");
-const singleEventRoutes = require("./singleEventRoute");
+const singleEventRoute = require("./singleEventRoute");
 const profileRoutes = require("./profileRoutes");
 
 router.use("/", homeRoutes);
 router.use("/account", accountRoutes);
 router.use("/api", apiRoutes);
-router.use("/poll", singleEventRoutes);
+router.use("/poll", singleEventRoute);
 router.use("/profile", profileRoutes);
 
 module.exports = router;
